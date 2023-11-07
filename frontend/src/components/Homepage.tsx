e7965987086d6b2316caef67e325580b514d0de0
James Michel-Williams <jmichelwilliams@gmail.com>
2023-11-07 16:59:56 -0500

    Fixed styling for mobile first

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -1,32 +1,32 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import Grid from '@mui/material/Grid'
 import styled from 'styled-components'
 import NavigationButton from './NavigationButton'
 
 const Description = styled.h2`
   margin: 8px;
   text-align: center;
+  width: 72vw;
 `
 const Homepage: React.FC = () => {
   return (
     <Grid
       container
-      sx={{
-        justifyContent: 'center',
-        flexDirection: 'column',
-        alignItems: 'center'
-      }}
+      spacing={8}
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="center"
     >
-      <Grid item xs={12}>
+      <Grid item sm={4}>
         <h1>Fit-App</h1>
       </Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} sm={8}>
         <Description>
           Tired of losing track of your workouts? Fear no more, Fit-App is here
           to help you!
         </Description>
       </Grid>
-      <Grid item xs={12} sx={{ marginTop: '64px' }}>
+      <Grid item sm={12} sx={{ marginTop: '64px' }}>
         <NavigationButton
           buttonText="Click here to start!"
           destination={'/planner'}
