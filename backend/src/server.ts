87bfc23f29b85ce4ac94b1ee53e11f27c0a86c88
James Michel-Williams <jmichelwilliams@gmail.com>
2024-03-30 17:57:37 -0400

    Implemented token validation on workouts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,26 @@
 import express, { type Request, type Response } from 'express';
 import morgan from 'morgan';
 import { getUserById, getAllProgramsForUser } from './api/user_handlers';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+const cors = require('cors');
 
 const app = express();
+const { auth } = require('express-oauth2-jwt-bearer');
+
+const checkJwt = auth({
+  audience: 'http://localhost:8000/user',
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  tokenSigningAlg: 'RS256',
+});
 
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.get('/user/:userId', checkJwt, getUserById);
+app.get('/user/:userId/programs', checkJwt, getAllProgramsForUser);
 
-app.get('/user/:userId', getUserById);
-app.get('/user/:userId/programs', getAllProgramsForUser);
 app.get('*', (req: Request, res: Response) => {
   res.status(404).json({
     status: 404,
