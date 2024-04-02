3790a97e617e2069a9494174e1b37cb265f51fa9
James Michel-Williams <jmichelwilliams@gmail.com>
2024-04-02 17:19:56 -0400

    extracted drawer and implemented logic

--- frontend/src/components/auth/Profile.tsx
+++ frontend/src/components/auth/Profile.tsx
@@ -1,33 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
-import styled from 'styled-components'
 import { Box } from '@mui/material'
-import Button from '@mui/material/Button'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
+import SwipeableTemporaryDrawer from './SwipeableDrawer'
 
-const StyledImg = styled.img`
-  height: 50px;
-  width: 50px;
-  border-radius: 16px;
-`
 const Profile: React.FC = () => {
   const { user, isAuthenticated, isLoading } = useAuth0()
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const { logout } = useAuth0()
 
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>): void => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = (): void => {
-    setAnchorEl(null)
-  }
-
-  const handleLogout = (): void => {
-    setAnchorEl(null)
-    void logout({ logoutParams: { returnTo: window.location.origin } })
-  }
   if (isLoading) {
     return <div>Loading ...</div>
   }
@@ -45,32 +23,7 @@ const Profile: React.FC = () => {
           justifyContent: 'flex-end'
         }}
       >
-        <Button
-          id="basic-button"
-          variant="text"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClick}
-        >
-          <Button id="usernickname" sx={{ margin: '0px' }}>
-            {' '}
-            <StyledImg src={user.picture} alt={user.name} />
-          </Button>
-        </Button>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button'
-          }}
-          sx={{ left: '16px' }}
-        >
-          {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
-          <MenuItem onClick={handleLogout}>Logout</MenuItem>
-        </Menu>
+        <SwipeableTemporaryDrawer />
       </Box>
     )
   )
