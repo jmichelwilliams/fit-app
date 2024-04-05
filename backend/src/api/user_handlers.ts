e9ab7a218f14c9a6d2da60978f4f4f633112c6d4
James Michel-Williams <jmichelwilliams@gmail.com>
2024-04-04 23:41:09 -0400

    housekeeping on userhandlers

--- backend/src/api/user_handlers.ts
+++ backend/src/api/user_handlers.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import { connectToDatabase } from '../database/db_connect';
-import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config({ path: '../.env' });
 
