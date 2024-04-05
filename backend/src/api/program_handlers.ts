645a0914999f72061f044c9592efd6d0cbe7e5ec
James Michel-Williams <jmichelwilliams@gmail.com>
2024-04-04 23:40:55 -0400

    addProgram added

--- backend/src/api/program_handlers.ts
+++ backend/src/api/program_handlers.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
 import { connectToDatabase } from '../database/db_connect';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config({ path: '../.env' });
 
 const { PROGRAMS_COLLECTION } = process.env;
 
+interface Exercise {
+  exerciseName: string;
+  sets: number;
+  reps: number;
+  rest: string;
+  weight?: number;
+}
+
+interface Program {
+  _id: string;
+  programName: string;
+  exercises: Exercise[];
+  createdBy: string;
+}
+
 export const getAllProgramsForUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
-  console.log('request:', req);
+
   if (!PROGRAMS_COLLECTION) {
     throw new Error('PROGRAMS_COLLECTION not defined');
   }
@@ -36,16 +52,32 @@ export const getAllProgramsForUser = async (req: Request, res: Response) => {
   }
 };
 
-// export const addProgram = async (req: Request, res: Response) => {
-//   const { userId, } = req.params;
+export const addProgram = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const { programName, exercises } = req.body.newProgram;
 
-//   if (!PROGRAMS_COLLECTION) {
-//     throw new Error('PROGRAMS_COLLECTION not defined');
-//   }
-//   const { client, db } = await connectToDatabase();
+  if (!PROGRAMS_COLLECTION) {
+    throw new Error('PROGRAMS_COLLECTION not defined');
+  }
+  const { client, db } = await connectToDatabase();
 
-//   try{
+  try {
+    const programCollection = db.collection(PROGRAMS_COLLECTION);
 
-//   }
+    const newProgram: Program = {
+      _id: uuidv4(),
+      programName,
+      exercises,
+      createdBy: userId,
+    };
 
-// };
+    const result = await programCollection.insertOne(newProgram as any);
+    res.status(200).json({ status: 200, data: result });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: 'Server error, try again' });
+  } finally {
+    if (client) {
+      await client.close();
+    }
+  }
+};
