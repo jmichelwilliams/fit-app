491145cc00168537bb499b0d749d1c5071c523cf
James Michel-Williams <jmichelwilliams@gmail.com>
2024-04-11 14:53:01 -0400

    Removed the fetch from workouts and put in ProgramList

--- frontend/src/components/ProgramList.tsx
+++ frontend/src/components/ProgramList.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type Program from '../types/Program'
 import { Box } from '@mui/material'
 import NavigationButton from './NavigationButton'
+import { useAuth0 } from '@auth0/auth0-react'
+import { fetchUserPrograms } from '../utils/fetchUserPrograms'
 
-interface ProgramListProps {
-  programs: Program[]
-}
+const ProgramList: React.FC = () => {
+  const [userPrograms, setUserPrograms] = useState<Program[]>([])
+  const { user, getAccessTokenSilently } = useAuth0()
 
-const ProgramList: React.FC<ProgramListProps> = ({ programs }) => {
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const accessToken = await getAccessTokenSilently()
+
+        const fetchedPrograms = await fetchUserPrograms(user?.sub, accessToken)
+        setUserPrograms(fetchedPrograms)
+      } catch (error) {
+        console.error('Error fetching programs:', error)
+      }
+    }
+    fetchData().catch((error) => {
+      console.error('Error fetching programs:', error)
+    })
+  }, [user, getAccessTokenSilently])
+
+  console.log('programs: ', userPrograms)
   return (
     <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
-      {programs.map((program) => {
+      {userPrograms.map((program) => {
         return (
           <Box key={program._id} sx={{ margin: '16px' }}>
             <NavigationButton
--- frontend/src/components/Workouts.tsx
+++ frontend/src/components/Workouts.tsx
@@ -1,32 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
-import { useAuth0 } from '@auth0/auth0-react'
-import type Program from '../types/Program'
-import { fetchUserPrograms } from '../utils/fetchUserPrograms'
 import ProgramList from './ProgramList'
 
 const Workouts: React.FC = () => {
-  const [programs, setPrograms] = useState<Program[]>([])
-  const { user, getAccessTokenSilently } = useAuth0()
-
-  useEffect(() => {
-    const fetchData = async (): Promise<void> => {
-      try {
-        const accessToken = await getAccessTokenSilently()
-
-        const fetchedPrograms = await fetchUserPrograms(user?.sub, accessToken)
-        setPrograms(fetchedPrograms)
-      } catch (error) {
-        console.error('Error fetching programs:', error)
-      }
-    }
-    fetchData().catch((error) => {
-      console.error('Error fetching programs:', error)
-    })
-  }, [user, getAccessTokenSilently])
-
-  console.log('programs: ', programs)
   return (
     <Box
       sx={{
@@ -40,7 +17,7 @@ const Workouts: React.FC = () => {
       <Typography variant="h3" textAlign="center">
         Workouts
       </Typography>
-      <ProgramList programs={programs} />
+      <ProgramList />
     </Box>
   )
 }
