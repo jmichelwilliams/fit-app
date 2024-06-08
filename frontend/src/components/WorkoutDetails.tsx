6a543a2a0406daaca268361ab71db07a31d47a50
James Michel-Williams <jmichelwilliams@gmail.com>
2024-06-07 23:26:12 -0400

    refactored workoutdetails to use RHF and modified workouthandler

--- frontend/src/components/WorkoutDetails.tsx
+++ frontend/src/components/WorkoutDetails.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react'
+import { useForm, Controller, type SubmitHandler } from 'react-hook-form'
 import useFetchProgram from '../hooks/useFetchProgram'
 import type Program from '../types/Program'
 import { useParams, useNavigate } from 'react-router-dom'
@@ -6,70 +8,54 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { fetchProgram } from '../utils/fetchProgram'
 import { formatRestTime } from '../utils/formatRestTime'
 import Box from '@mui/material/Box'
-import { Button } from '@mui/material'
-import Typography from '@mui/material/Typography'
-import Checkbox from '@mui/material/Checkbox'
-import TextField from '@mui/material/TextField'
-import Grid from '@mui/material/Grid'
-import useMediaQuery from '@mui/material/useMediaQuery'
+import {
+  Button,
+  Typography,
+  Checkbox,
+  TextField,
+  Grid,
+  useMediaQuery,
+  FormControlLabel
+} from '@mui/material'
 import useTheme from '@mui/material/styles/useTheme'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import Footer from './Footer'
 
+interface ProgramFormInputs {
+  exercises: Array<{
+    exerciseName: string
+    sets: Array<{ setId: number; reps: number }>
+    weight: number
+    completed: boolean
+  }>
+}
 const WorkoutDetails: React.FC = () => {
-  const [completed, setCompleted] = useState<boolean[]>([])
   const [program, setProgram] = useState<Program>()
-  const [workoutSession, setWorkoutSession] = useState<Program>()
+  const { user, getAccessTokenSilently } = useAuth0()
   const { programId } = useParams<{
     programId: string
   }>()
-  const { user, getAccessTokenSilently } = useAuth0()
+  useFetchProgram(programId, getAccessTokenSilently, fetchProgram, setProgram)
+  const { control, handleSubmit, watch } = useForm<ProgramFormInputs>({
+    mode: 'onBlur'
+  })
+  const navigate = useNavigate()
   const theme = useTheme()
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
-  const navigate = useNavigate()
-  useFetchProgram(programId, getAccessTokenSilently, fetchProgram, setProgram)
 
-  useEffect(() => {
-    setWorkoutSession(program)
-  }, [program])
-
-  const handleToggle = (index: number): void => {
-    const newCompleted = [...completed]
-    newCompleted[index] = !completed[index]
-    setCompleted(newCompleted)
-  }
-
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    index: number,
-    exercise: any
-  ): void => {
-    const { name, value } = event.target
-
-    if (workoutSession !== undefined) {
-      const updatedWorkout = { ...workoutSession }
-      const updatedExercise = { ...exercise }
-
-      if (name === 'reps') {
-        updatedExercise.sets[index] = {
-          ...updatedExercise.sets[index],
-          reps: parseInt(value)
-        }
-      }
-
-      if (name === 'weight') {
-        updatedExercise.weight = parseInt(value, 10)
-      }
-
-      updatedWorkout.exercises[index] = updatedExercise
-      setWorkoutSession(updatedWorkout as Program)
-    }
-  }
-
-  const handleSubmit = async (): Promise<void> => {
+  const onSubmit: SubmitHandler<ProgramFormInputs> = async (
+    data
+  ): Promise<void> => {
     try {
       const accessToken = await getAccessTokenSilently()
 
+      const completeData = {
+        ...data,
+        exercises: data.exercises.map((exercise, index) => ({
+          ...exercise,
+          exerciseName: program?.exercises[index].exerciseName,
+          rest: program?.exercises[index].rest
+        }))
+      }
       if (user !== null && user !== undefined) {
         const response = await fetch(`/workouts/${user.sub}`, {
           method: 'POST',
@@ -77,7 +63,7 @@ const WorkoutDetails: React.FC = () => {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${accessToken}`
           },
-          body: JSON.stringify({ workoutSession })
+          body: JSON.stringify({ workoutSession: completeData })
         })
 
         if (response.ok) {
@@ -90,7 +76,7 @@ const WorkoutDetails: React.FC = () => {
       console.error('Error', error)
     }
   }
-
+  console.log('watch(): ', watch())
   return (
     <Box
       sx={{
@@ -101,167 +87,234 @@ const WorkoutDetails: React.FC = () => {
         paddingBottom: '32px'
       }}
     >
-      {program?.exercises.map((exercise, exerciseIndex) => {
-        return (
-          <Box
-            key={`exercise-${exerciseIndex}`}
-            sx={{
-              border: '4px solid black',
-              width: '90vw',
-              height: 'auto',
-              margin: '16px',
-              padding: '8px',
-              borderRadius: '16px',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
-          >
-            <Typography
-              variant="subtitle1"
-              fontWeight="bold"
-              textAlign="center"
-              sx={{ marginBottom: '8px' }}
-            >
-              {exercise.exerciseName}
-            </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {program?.exercises.map((exercise, exerciseIndex) => {
+          return (
             <Box
+              key={`exercise-${exerciseIndex}`}
               sx={{
+                border: '4px solid black',
+                width: '90vw',
+                height: 'auto',
+                margin: '16px',
+                padding: '8px',
+                borderRadius: '16px',
                 display: 'flex',
                 flexDirection: 'column',
-                justifyContent: 'space-around',
                 alignItems: 'center'
               }}
             >
-              <TextField
-                id={`weightInput-${exerciseIndex}`}
-                label="weight (lbs)"
-                name="weight"
-                type="number"
-                size="small"
-                defaultValue={exercise.weight}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  handleChange(e, exerciseIndex, exercise)
-                }}
-                inputProps={{
-                  inputMode: 'numeric',
-                  pattern: '[0-9]*'
-                }}
-                InputLabelProps={{
-                  sx: { fontSize: isSmallScreen ? '.95rem' : '1rem' }
-                }}
-                sx={{
-                  width: '30%'
-                }}
-              />
+              <Typography
+                variant="subtitle1"
+                fontWeight="bold"
+                textAlign="center"
+                sx={{ marginBottom: '8px' }}
+              >
+                {exercise.exerciseName}
+              </Typography>
               <Box
                 sx={{
                   display: 'flex',
-                  justifyContent: 'space-evenly',
-                  flexWrap: 'wrap'
+                  flexDirection: 'column',
+                  justifyContent: 'space-around',
+                  alignItems: 'center'
                 }}
               >
-                {Array.from({ length: exercise.sets.length }, (_, setIndex) => {
-                  return (
-                    <Box
-                      key={`set-${exerciseIndex}-${setIndex + 1}`}
-                      sx={{
-                        display: 'flex',
-                        margin: '8px',
-                        flexDirection: 'column',
-                        maxWidth: '80px',
-                        alignItems: 'space-evenly'
+                <Controller
+                  name={`exercises.${exerciseIndex}.weight`}
+                  control={control}
+                  defaultValue={exercise.weight}
+                  rules={{
+                    required: 'Weight is required',
+                    min: { value: 1, message: 'Minimum value is 1' }
+                  }}
+                  render={({
+                    field: { onChange, value, ref, onBlur },
+                    fieldState: { error }
+                  }) => (
+                    <TextField
+                      id={`weightInput-${exerciseIndex}`}
+                      label="weight (lbs)"
+                      name="weight"
+                      type="number"
+                      size="small"
+                      value={value}
+                      onBlur={onBlur}
+                      onChange={(e) => {
+                        const val = parseInt(e.target.value)
+                        onChange(val)
                       }}
-                    >
-                      <Typography variant="body1" textAlign="center">
-                        Set: {setIndex + 1}
-                      </Typography>
-                      <Grid
-                        container
-                        spacing={2}
-                        justifyContent="center"
-                        alignItems="center"
-                        sx={{ paddingLeft: '8px' }}
-                      >
-                        <Grid
-                          item
-                          xs={12}
+                      inputRef={ref}
+                      onKeyDown={(e) => {
+                        if (['e', '-', '+'].includes(e.key)) {
+                          e.preventDefault()
+                        }
+                      }}
+                      inputProps={{
+                        inputMode: 'numeric',
+                        pattern: '[0-9]*'
+                      }}
+                      InputLabelProps={{
+                        sx: { fontSize: isSmallScreen ? '.95rem' : '1rem' }
+                      }}
+                      sx={{
+                        width: '30%'
+                      }}
+                      error={!(error == null)}
+                      helperText={error != null ? error.message : null}
+                    />
+                  )}
+                />
+                <Box
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly',
+                    flexWrap: 'wrap'
+                  }}
+                >
+                  {Array.from(
+                    { length: exercise.sets.length },
+                    (_, setIndex) => {
+                      return (
+                        <Box
+                          key={`set-${exerciseIndex}-${setIndex + 1}`}
                           sx={{
                             display: 'flex',
-                            justifyContent: 'center',
-                            margin: '8px'
+                            margin: '8px',
+                            flexDirection: 'column',
+                            maxWidth: '80px',
+                            alignItems: 'space-evenly'
                           }}
                         >
-                          <TextField
-                            id={`repsInput-${exerciseIndex}-${setIndex + 1}`}
-                            label="reps"
-                            name="reps"
-                            type="number"
-                            size="small"
-                            defaultValue={exercise.sets[setIndex].reps}
-                            onChange={(
-                              e: React.ChangeEvent<HTMLInputElement>
-                            ) => {
-                              handleChange(e, setIndex, exercise)
-                            }}
-                            inputProps={{
-                              inputMode: 'numeric',
-                              pattern: '[0-9]*'
-                            }}
-                            InputLabelProps={{
-                              sx: {
-                                fontSize: isSmallScreen ? '.95rem' : '1rem',
-                                textAlign: 'center'
-                              }
-                            }}
-                            sx={{
-                              width: '100%'
-                            }}
-                          />
-                        </Grid>
-                      </Grid>
-                    </Box>
-                  )
-                })}
+                          <Typography variant="body1" textAlign="center">
+                            Set: {setIndex + 1}
+                          </Typography>
+                          <Grid
+                            container
+                            spacing={2}
+                            justifyContent="center"
+                            alignItems="center"
+                            sx={{ paddingLeft: '8px' }}
+                          >
+                            <Grid
+                              item
+                              xs={12}
+                              sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                margin: '8px'
+                              }}
+                            >
+                              <Controller
+                                name={`exercises.${exerciseIndex}.sets.${setIndex}.reps`}
+                                control={control}
+                                defaultValue={exercise.sets[setIndex].reps}
+                                rules={{
+                                  required: 'Rep is required',
+                                  min: {
+                                    value: 1,
+                                    message: 'Minimum value is 1'
+                                  }
+                                }}
+                                render={({
+                                  field: { onChange, value, ref, onBlur },
+                                  fieldState: { error }
+                                }) => (
+                                  <TextField
+                                    id={`repsInput-${exerciseIndex}-${
+                                      setIndex + 1
+                                    }`}
+                                    label="reps"
+                                    name="reps"
+                                    type="number"
+                                    size="small"
+                                    value={isNaN(value) ? '' : value}
+                                    inputRef={ref}
+                                    onBlur={onBlur}
+                                    onChange={(e) => {
+                                      const val = parseInt(e.target.value)
+
+                                      onChange(val)
+                                    }}
+                                    onKeyDown={(e) => {
+                                      if (['e', '-', '+'].includes(e.key)) {
+                                        e.preventDefault()
+                                      }
+                                    }}
+                                    inputProps={{
+                                      inputMode: 'numeric',
+                                      pattern: '[0-9]*'
+                                    }}
+                                    InputLabelProps={{
+                                      sx: {
+                                        fontSize: isSmallScreen
+                                          ? '.95rem'
+                                          : '1rem',
+                                        textAlign: 'center'
+                                      }
+                                    }}
+                                    sx={{
+                                      width: '100%'
+                                    }}
+                                    error={!(error == null)}
+                                    helperText={
+                                      error != null ? error.message : null
+                                    }
+                                  />
+                                )}
+                              />
+                            </Grid>
+                          </Grid>
+                        </Box>
+                      )
+                    }
+                  )}
+                </Box>
+                <Typography
+                  variant="body1"
+                  textAlign="center"
+                  sx={{ marginTop: '8px' }}
+                >
+                  Rest: {formatRestTime(exercise.rest)}
+                </Typography>
+                <Controller
+                  name={`exercises.${exerciseIndex}.completed`}
+                  control={control}
+                  defaultValue={false}
+                  render={({ field: { onChange, value, ref } }) => (
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          id={`completed-checkbox-${exerciseIndex}`}
+                          checked={value || false}
+                          onChange={() => {
+                            if (!value) {
+                              onChange(true)
+                            } else {
+                              onChange(false)
+                            }
+                          }}
+                          style={{
+                            color: value ? 'green' : 'initial'
+                          }}
+                        />
+                      }
+                      label="Completed?"
+                    />
+                  )}
+                />
               </Box>
-              <Typography
-                variant="body1"
-                textAlign="center"
-                sx={{ marginTop: '8px' }}
-              >
-                Rest: {formatRestTime(exercise.rest)}
-              </Typography>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    id={`completed-checkbox-${exerciseIndex}`}
-                    checked={completed[exerciseIndex] || false}
-                    onChange={() => {
-                      handleToggle(exerciseIndex)
-                    }}
-                    style={{
-                      color: completed[exerciseIndex] ? 'green' : 'initial'
-                    }}
-                  />
-                }
-                label="Completed?"
-              />
             </Box>
-          </Box>
-        )
-      })}
-      <Box sx={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 100 }}>
-        <Footer>
-          <Button
-            variant={'contained'}
-            onClick={() => {
-              void handleSubmit()
-            }}
-          >
-            Complete Workout
-          </Button>
-        </Footer>
-      </Box>
+          )
+        })}
+        <Box sx={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 100 }}>
+          <Footer>
+            <Button variant={'contained'} type="submit">
+              Complete Workout
+            </Button>
+          </Footer>
+        </Box>
+      </form>
     </Box>
   )
 }
