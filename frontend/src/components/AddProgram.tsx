0495b90c904a33a96ceffa142d782c71c6aca003
James Michel-Williams <jmichelwilliams@gmail.com>
2024-06-18 20:12:40 -0400

    adjusted spacer for validation text

--- frontend/src/components/AddProgram.tsx
+++ frontend/src/components/AddProgram.tsx
@@ -119,7 +119,7 @@ const AddProgram: React.FC = () => {
               sx={{
                 display: 'flex',
                 justifyContent: 'center',
-                height: '100px'
+                height: '90px'
               }}
             >
               <Controller
@@ -150,9 +150,6 @@ const AddProgram: React.FC = () => {
                     }}
                     error={!(error == null)}
                     helperText={error != null ? error.message : null}
-                    FormHelperTextProps={{
-                      sx: { height: '2em' }
-                    }}
                   />
                 )}
               />
@@ -172,190 +169,214 @@ const AddProgram: React.FC = () => {
                   }}
                 >
                   <Box display="flex" justifyContent="center">
-                    <Typography
-                      variant="subtitle1"
-                      sx={{ textAlign: 'center' }}
-                    >
+                    <Typography variant="h5" sx={{ textAlign: 'center' }}>
                       Exercise {exerciseIndex + 1}
                     </Typography>
                   </Box>
-                  <Controller
-                    name={`exercises.${exerciseIndex}.exerciseName`}
-                    control={control}
-                    rules={{
-                      required: 'A name is required',
-                      minLength: { value: 3, message: 'Minimum length is 3' }
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      height: '90px'
                     }}
-                    render={({
-                      field: { onChange, value, ref, onBlur },
-                      fieldState: { error }
-                    }) => (
-                      <TextField
-                        label="Name of Exercise"
-                        id={`exercises[${exerciseIndex}]`}
-                        value={value}
-                        onBlur={onBlur}
-                        inputRef={ref}
-                        onChange={(e) => {
-                          const val = e.target.value
-                          onChange(val)
-                        }}
-                        required
-                        sx={{
-                          width: '85vw',
-                          margin: '8px'
-                        }}
-                        error={!(error == null)}
-                        helperText={error != null ? error.message : null}
-                      />
-                    )}
-                  />
-                  <Box sx={{ width: '290px' }}>
+                  >
                     <Controller
-                      name={`exercises.${exerciseIndex}.sets`}
+                      name={`exercises.${exerciseIndex}.exerciseName`}
                       control={control}
-                      render={({ field: { onChange, value } }) => (
-                        <FormControl>
-                          <InputLabel
-                            id={`sets-label-${exerciseIndex}`}
-                            sx={{ margin: '8px' }}
-                          >
-                            Sets
-                          </InputLabel>
-                          <Select
-                            id={`exercises.${exerciseIndex}.sets`}
-                            labelId={`sets-label-${exerciseIndex}`}
-                            label="Sets"
-                            value={value}
-                            sx={{ margin: '8px', width: '80px' }}
-                            onChange={(e) => {
-                              const setsCount = e.target.value as number
-                              onChange(setsCount)
-                            }}
-                          >
-                            {Array.from({ length: 5 }, (_, i) => i + 1).map(
-                              (set) => (
-                                <MenuItem key={set} value={set}>
-                                  {set}
-                                </MenuItem>
-                              )
-                            )}
-                          </Select>
-                        </FormControl>
-                      )}
-                    />
-                    <Controller
-                      name={`exercises.${exerciseIndex}.reps`}
-                      control={control}
-                      render={({ field: { onChange, value } }) => (
-                        <FormControl>
-                          <InputLabel
-                            id={`reps-label-${exerciseIndex}`}
-                            sx={{ margin: '8px' }}
-                          >
-                            Reps
-                          </InputLabel>
-                          <Select
-                            id={`exercises.${exerciseIndex}.reps`}
-                            labelId={`reps-label-${exerciseIndex}`}
-                            label="Reps"
-                            value={value}
-                            sx={{ margin: '8px', width: '80px' }}
-                            onChange={(e) => {
-                              const repsCount = e.target.value as number
-                              onChange(repsCount)
-                            }}
-                          >
-                            {Array.from({ length: 15 }, (_, i) => i + 1).map(
-                              (rep) => (
-                                <MenuItem key={rep} value={rep}>
-                                  {rep}
-                                </MenuItem>
-                              )
-                            )}
-                          </Select>
-                        </FormControl>
-                      )}
-                    />
-                    <Controller
-                      name={`exercises.${exerciseIndex}.rest`}
-                      control={control}
-                      render={({ field: { onChange, value } }) => (
-                        <FormControl>
-                          <InputLabel
-                            id={`rest-label-${exerciseIndex}`}
-                            sx={{ margin: '8px' }}
-                          >
-                            Rest
-                          </InputLabel>
-                          <Select
-                            id={`exercises.${exerciseIndex}.rest`}
-                            labelId={`rest-label-${exerciseIndex}`}
-                            label="Rest"
-                            value={value}
-                            sx={{ margin: '8px', width: '80px' }}
-                            onChange={(e) => {
-                              const restTime = e.target.value
-                              onChange(restTime)
-                            }}
-                          >
-                            <MenuItem value={'0:30'}>0:30</MenuItem>
-                            <MenuItem value={'1:00'}>1:00</MenuItem>
-                            <MenuItem value={'1:30'}>1:30</MenuItem>
-                            <MenuItem value={'2:00'}>2:00</MenuItem>
-                          </Select>
-                        </FormControl>
+                      rules={{
+                        required: 'A name is required',
+                        minLength: { value: 3, message: 'Minimum length is 3' }
+                      }}
+                      render={({
+                        field: { onChange, value, ref, onBlur },
+                        fieldState: { error }
+                      }) => (
+                        <TextField
+                          label="Name of Exercise"
+                          id={`exercises[${exerciseIndex}]`}
+                          value={value}
+                          onBlur={onBlur}
+                          inputRef={ref}
+                          onChange={(e) => {
+                            const val = e.target.value
+                            onChange(val)
+                          }}
+                          required
+                          sx={{
+                            width: '85vw',
+                            margin: '8px'
+                          }}
+                          error={!(error == null)}
+                          helperText={error != null ? error.message : null}
+                        />
                       )}
                     />
                   </Box>
-                  <Controller
-                    name={`exercises.${exerciseIndex}.weight`}
-                    control={control}
-                    rules={{
-                      required: 'Weight is required',
-                      min: { value: 1, message: 'Minimum value is 1' }
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      height: '80px'
                     }}
-                    render={({
-                      field: { onChange, value, ref, onBlur },
-                      fieldState: { error }
-                    }) => (
-                      <TextField
-                        id={`exercises.${exerciseIndex}.weight`}
-                        label="weight"
-                        name={`exercises.${exerciseIndex}.weight`}
-                        type="number"
-                        size="small"
-                        value={value}
-                        onBlur={onBlur}
-                        onChange={(e) => {
-                          const val = parseFloat(e.target.value)
-                          if (!isNaN(val) && val !== 0) {
-                            onChange(val)
-                          } else {
-                            onChange('')
-                          }
-                        }}
-                        inputRef={ref}
-                        onKeyDown={(e) => {
-                          if (['e', '-', '+'].includes(e.key)) {
-                            e.preventDefault()
-                          }
-                        }}
-                        InputProps={{
-                          inputMode: 'decimal',
-                          endAdornment: (
-                            <InputAdornment position="end">lbs</InputAdornment>
-                          )
-                        }}
-                        sx={{
-                          width: '120px',
-                          margin: '8px'
-                        }}
-                        error={!(error == null)}
-                        helperText={error != null ? error.message : null}
+                  >
+                    <Box sx={{ width: '290px' }}>
+                      <Controller
+                        name={`exercises.${exerciseIndex}.sets`}
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                          <FormControl>
+                            <InputLabel
+                              id={`sets-label-${exerciseIndex}`}
+                              sx={{ margin: '8px' }}
+                            >
+                              Sets
+                            </InputLabel>
+                            <Select
+                              id={`exercises.${exerciseIndex}.sets`}
+                              labelId={`sets-label-${exerciseIndex}`}
+                              label="Sets"
+                              value={value}
+                              sx={{ margin: '8px', width: '80px' }}
+                              onChange={(e) => {
+                                const setsCount = e.target.value as number
+                                onChange(setsCount)
+                              }}
+                            >
+                              {Array.from({ length: 5 }, (_, i) => i + 1).map(
+                                (set) => (
+                                  <MenuItem key={set} value={set}>
+                                    {set}
+                                  </MenuItem>
+                                )
+                              )}
+                            </Select>
+                          </FormControl>
+                        )}
                       />
-                    )}
-                  />
+                      <Controller
+                        name={`exercises.${exerciseIndex}.reps`}
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                          <FormControl>
+                            <InputLabel
+                              id={`reps-label-${exerciseIndex}`}
+                              sx={{ margin: '8px' }}
+                            >
+                              Reps
+                            </InputLabel>
+                            <Select
+                              id={`exercises.${exerciseIndex}.reps`}
+                              labelId={`reps-label-${exerciseIndex}`}
+                              label="Reps"
+                              value={value}
+                              sx={{ margin: '8px', width: '80px' }}
+                              onChange={(e) => {
+                                const repsCount = e.target.value as number
+                                onChange(repsCount)
+                              }}
+                            >
+                              {Array.from({ length: 15 }, (_, i) => i + 1).map(
+                                (rep) => (
+                                  <MenuItem key={rep} value={rep}>
+                                    {rep}
+                                  </MenuItem>
+                                )
+                              )}
+                            </Select>
+                          </FormControl>
+                        )}
+                      />
+                      <Controller
+                        name={`exercises.${exerciseIndex}.rest`}
+                        control={control}
+                        render={({ field: { onChange, value } }) => (
+                          <FormControl>
+                            <InputLabel
+                              id={`rest-label-${exerciseIndex}`}
+                              sx={{ margin: '8px' }}
+                            >
+                              Rest
+                            </InputLabel>
+                            <Select
+                              id={`exercises.${exerciseIndex}.rest`}
+                              labelId={`rest-label-${exerciseIndex}`}
+                              label="Rest"
+                              value={value}
+                              sx={{ margin: '8px', width: '80px' }}
+                              onChange={(e) => {
+                                const restTime = e.target.value
+                                onChange(restTime)
+                              }}
+                            >
+                              <MenuItem value={'0:30'}>0:30</MenuItem>
+                              <MenuItem value={'1:00'}>1:00</MenuItem>
+                              <MenuItem value={'1:30'}>1:30</MenuItem>
+                              <MenuItem value={'2:00'}>2:00</MenuItem>
+                            </Select>
+                          </FormControl>
+                        )}
+                      />
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      height: '90px'
+                    }}
+                  >
+                    <Controller
+                      name={`exercises.${exerciseIndex}.weight`}
+                      control={control}
+                      rules={{
+                        required: 'Weight is required',
+                        min: { value: 1, message: 'Minimum value is 1' }
+                      }}
+                      render={({
+                        field: { onChange, value, ref, onBlur },
+                        fieldState: { error }
+                      }) => (
+                        <TextField
+                          id={`exercises.${exerciseIndex}.weight`}
+                          label="weight"
+                          name={`exercises.${exerciseIndex}.weight`}
+                          type="number"
+                          size="small"
+                          value={value}
+                          onBlur={onBlur}
+                          onChange={(e) => {
+                            const val = parseFloat(e.target.value)
+                            if (!isNaN(val) && val !== 0) {
+                              onChange(val)
+                            } else {
+                              onChange('')
+                            }
+                          }}
+                          inputRef={ref}
+                          onKeyDown={(e) => {
+                            if (['e', '-', '+'].includes(e.key)) {
+                              e.preventDefault()
+                            }
+                          }}
+                          InputProps={{
+                            inputMode: 'decimal',
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                lbs
+                              </InputAdornment>
+                            )
+                          }}
+                          sx={{
+                            width: '120px',
+                            height: '65px',
+                            margin: '8px'
+                          }}
+                          error={!(error == null)}
+                          helperText={error != null ? error.message : null}
+                        />
+                      )}
+                    />
+                  </Box>
                   <Box
                     sx={{
                       display: 'flex',
