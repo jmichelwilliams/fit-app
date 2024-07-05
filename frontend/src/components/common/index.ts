598f31543025f336123198248f25bfe39c6408e7
James Michel-Williams <jmichelwilliams@gmail.com>
2024-07-05 14:19:45 -0400

    extracted programName

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -3,5 +3,13 @@ import { Footer } from './Footer'
 import { Header } from './Header'
 import { NavigationButton } from './NavigationButton'
 import { Drawer } from './Drawer'
+import { ProgramNameField } from './ProgramNameField'
 
-export { BackButton, Footer, Header, NavigationButton, Drawer }
+export {
+  BackButton,
+  Footer,
+  Header,
+  NavigationButton,
+  Drawer,
+  ProgramNameField
+}
