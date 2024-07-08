15ef3c43d80812a1e6d8e353ee523cb6373e2ff5
James Michel-Williams <jmichelwilliams@gmail.com>
2024-07-08 15:54:22 -0400

    integrated snackbar context and notifications

---
+++ frontend/src/components/common/SnackbarNotification.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Snackbar from '@mui/material/Snackbar'
+import Alert from '@mui/material/Alert'
+import { useSnackbar } from 'context/SnackbarContext'
+
+export const SnackbarNotification: React.FC = () => {
+  const { open, message, severity, closeSnackbar } = useSnackbar()
+
+  return (
+    <Box sx={{ width: 500 }}>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        autoHideDuration={5000}
+        open={open}
+        onClose={closeSnackbar}
+        sx={{ zIndex: 1300 }}
+      >
+        <Alert severity={severity} variant="filled" sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </Box>
+  )
+}
