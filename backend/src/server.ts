118e37f39df46347bdee9341a8e6dff1787b866d
James Michel-Williams <jmichelwilliams@gmail.com>
2024-07-09 23:03:22 -0400

    fixed missing types for cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -33,11 +33,14 @@ const allowedOrigins = [
 ];
 
 const corsOptions: CorsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void,
+  ) => {
+    if (allowedOrigins.includes(origin as string) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS'), false);
     }
   },
 };
