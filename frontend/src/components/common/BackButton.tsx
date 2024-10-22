fd957cd539251d3cbacf70503bb870bcb6a28697
James Michel-Williams <jmichelwilliams@gmail.com>
2024-10-22 15:52:38 -0400

    Fixed helper text and navigation button logic

--- frontend/src/components/common/BackButton.tsx
+++ frontend/src/components/common/BackButton.tsx
@@ -9,34 +9,53 @@ export const BackButton: React.FC = () => {
   const handleBack = (): void => {
     const id = location.pathname.split('/')[2]
 
-    switch (true) {
-      case location.pathname.includes(`/programs/${id}`):
-      case location.pathname.includes(`/workouts/${id}`):
+    switch (location.pathname) {
+      case `/programs/${id}`:
         navigate('/planner')
         break
-      case location.pathname.includes(`/workouts/history`):
-      case location.pathname.includes(`/demo/workouts/history`):
-      case location.pathname.includes(`/demo/planner`):
-      case location.pathname.includes(`/demo/workouts`):
-      case location.pathname.includes(`/planner`):
+      case `/workouts/${id}`:
+        navigate('/workouts')
+        break
+      case `/demo/programs/${id}`:
+        navigate('/demo/planner')
+        break
+      case `/demo/workouts/${id}`:
+        navigate('/demo/workouts')
+        break
+      case '/planner':
         navigate('/')
         break
-      case location.pathname.includes(`/demo/programs/${id}`):
-      case location.pathname.includes(`/demo/workouts/${id}`):
-        navigate('/demo/planner')
+      case '/workouts':
+        navigate('/')
+        break
+      case '/workouts/history':
+        navigate('/')
+        break
+      case '/demo':
+        navigate('/')
+        break
+      case '/demo/planner':
+        navigate('/demo')
+        break
+      case '/demo/workouts':
+        navigate('/demo')
+        break
+      case '/demo/workouts/history':
+        navigate('/demo')
         break
       default:
         navigate(-1)
     }
   }
-
   return (
-    <Button
-      variant="text"
-      onClick={handleBack}
-      sx={{ color: 'var(--font-color) !important' }}
-    >
-      Back
-    </Button>
+    <>
+      <Button
+        variant="text"
+        onClick={handleBack}
+        sx={{ color: 'var( --font-color) !important' }}
+      >
+        Back
+      </Button>
+    </>
   )
 }
